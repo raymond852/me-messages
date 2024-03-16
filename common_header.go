831deee9c56f0ccd1054cd722d830703b7ad3d6a
raymonddu <raymond.du.852@gmail.com>
2024-03-16 16:33:21 +0800

    add getter

--- common_header.go
+++ common_header.go
@@ -18,34 +18,69 @@ func (z *CommonHeader) SetSourceKafkaTopic(topicId int8) {
 	z.SourceKafkaTopic = topicId
 }
 
+func (z *CommonHeader) GetSourceKafkaTopic() int8 {
+	return z.SourceKafkaTopic
+}
+
 func (z *CommonHeader) SetSourceKafkaOffset(offset int64) {
 	z.SourceKafkaOffset = offset
+}
+
+func (z *CommonHeader) GetSourceKafkaOffset() int64 {
+	return z.SourceKafkaOffset
 }
 
 func (z *CommonHeader) SetSourceSeqNum(seqNum int64) {
 	z.SourceSeqNum = seqNum
 }
 
+func (z *CommonHeader) GetSourceSeqNum() int64 {
+	return z.SourceSeqNum
+}
+
 func (z *CommonHeader) SetSeqNum(seqNum int64) {
 	z.SeqNum = seqNum
+}
+
+func (z *CommonHeader) GetSeqNum() int64 {
+	return z.SeqNum
 }
 
 func (z *CommonHeader) SetKafkaOffset(offset int64) {
 	z.KafkaOffset = offset
 }
 
+func (z *CommonHeader) GetKafkaOffset() int64 {
+	return z.KafkaOffset
+}
+
 func (z *CommonHeader) SetTransactionId(transId int64) {
 	z.TransactionId = transId
+}
+
+func (z *CommonHeader) GetTransactionId() int64 {
+	return z.TransactionId
 }
 
 func (z *CommonHeader) SetIsLastTransaction(isLastTransaction bool) {
 	z.IsLastTransaction = isLastTransaction
 }
 
+func (z *CommonHeader) GetIsLastTransaction() bool {
+	return z.IsLastTransaction
+}
+
 func (z *CommonHeader) SetInputTime(inputTime int64) {
 	z.InputTime = inputTime
 }
 
+func (z *CommonHeader) GetInputTime() int64 {
+	return z.InputTime
+}
+
 func (z *CommonHeader) SetSendTime(sendTime int64) {
 	z.SendTime = sendTime
+}
+func (z *CommonHeader) GetSendTime() int64 {
+	return z.SendTime
 }
--- common_message.go
+++ common_message.go
@@ -2,12 +2,29 @@ package messages
 
 type CommonMessage interface {
 	SetSourceKafkaTopic(topicId int8)
+	GetSourceKafkaTopic() int8
+
 	SetSourceKafkaOffset(offset int64)
+	GetSourceKafkaOffset() int64
+
 	SetSourceSeqNum(seqNum int64)
+	GetSourceSeqNum() int64
+
 	SetSeqNum(seqNum int64)
+	GetSeqNum() int64
+
 	SetKafkaOffset(offset int64)
+	GetKafkaOffset() int64
+
 	SetTransactionId(transId int64)
+	GetTransactionId() int64
+
 	SetIsLastTransaction(isLastTransaction bool)
+	GetIsLastTransaction() bool
+
 	SetInputTime(inputTime int64)
+	GetInputTime() int64
+
 	SetSendTime(sendTime int64)
+	GetSendTime() int64
 }

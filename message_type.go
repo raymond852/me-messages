cc5ffe0421c9e1cc60a70c50a0c2eedd9b546d50
Raymond Du <raymond.du.852@gmail.com>
2024-11-11 20:04:15 +0800

    update message types

--- message_type.go
+++ message_type.go
@@ -1,19 +1,27 @@
 package messages
 
 var MessageTypeEnum = struct {
-	PlaceOrder      int8
-	ExecutionReport int8
-	Symbol          int8
-	Asset           int8
-	BalanceAdjust   int8
-	BalanceUpdate   int8
-	CodeError       int8
+	PlaceOrder                 int8
+	ExecutionReport            int8
+	Symbol                     int8
+	Asset                      int8
+	BalanceAdjust              int8
+	BalanceUpdate              int8
+	UserTierCommission         int8
+	DeleteUserTierCommission   int8
+	SymbolTierCommission       int8
+	DeleteSymbolTierCommission int8
+	CodeError                  int8
 }{
-	PlaceOrder:      1,
-	ExecutionReport: 2,
-	Symbol:          3,
-	Asset:           4,
-	BalanceAdjust:   5,
-	BalanceUpdate:   6,
-	CodeError:       100,
+	PlaceOrder:                 1,
+	ExecutionReport:            2,
+	Symbol:                     3,
+	Asset:                      4,
+	BalanceAdjust:              5,
+	BalanceUpdate:              6,
+	UserTierCommission:         7,
+	DeleteUserTierCommission:   8,
+	SymbolTierCommission:       9,
+	DeleteSymbolTierCommission: 10,
+	CodeError:                  100,
 }
